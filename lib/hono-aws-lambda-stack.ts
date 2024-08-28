9b915dec9364e72ffabecbdca5161e28129c9c49
seitamuro <seitamuro0210@gmail.com>
2024-08-28 21:59:20 +0900

    GET '/'

--- lib/hono-aws-lambda-stack.ts
+++ lib/hono-aws-lambda-stack.ts
@@ -1,16 +1,24 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class HonoAwsLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HonoAwsLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const fn = new NodejsFunction(this, "HonoFunction", {
+      entry: "lambda/index.ts",
+      handler: "handler",
+      runtime: lambda.Runtime.NODEJS_20_X,
+    });
+    fn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+    });
+    new apigw.LambdaRestApi(this, "HonoApi", {
+      handler: fn,
+    });
   }
 }
