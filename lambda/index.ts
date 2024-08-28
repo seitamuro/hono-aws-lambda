06f0d4fba47eb47c8ce3c09345b5d8b22c061de0
seitamuro <seitamuro0210@gmail.com>
2024-08-28 22:06:15 +0900

    lambda固有のコンテキストを受け取る

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,8 +1,29 @@
 import { Hono } from "hono";
+import type { LambdaContext, LambdaEvent } from "hono/aws-lambda";
 import { handle } from "hono/aws-lambda";
 
-const app = new Hono();
+type Bindings = {
+  event: LambdaEvent;
+  lambdaContext: LambdaContext;
+};
 
+const app = new Hono<{ Bindings: Bindings }>();
+
+// Hello, World!
 app.get("/", (c) => c.text("Hello, World!"));
+
+// Access AWS Lambda Object
+app.get("/aws-lambda-info/", (c) => {
+  return c.json({
+    isBase64Encoded: c.env.event.isBase64Encoded,
+    awsRequestId: c.env.lambdaContext.awsRequestId,
+  });
+});
+
+// Access RequestContext
+app.get("/request-context/", (c) => {
+  const lambdaContext = c.env.event.requestContext;
+  return c.json(lambdaContext);
+});
 
 export const handler = handle(app);
